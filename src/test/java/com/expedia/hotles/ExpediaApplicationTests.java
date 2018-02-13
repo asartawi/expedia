116ca8554b0385d22f08fc04af2aaa2f4ffaec31
abdulhafeth <abd.sarta@gmail.com>
2018-02-14 00:02:35 +0200

    Adding two tests

--- src/test/java/com/expedia/hotles/ExpediaApplicationTests.java
+++ src/test/java/com/expedia/hotles/ExpediaApplicationTests.java
@@ -1,18 +1,51 @@
 package com.expedia.hotles;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.expedia.hotels.ExpediaApplication;
+import com.expedia.hotels.service.OffersService;
+import com.expedia.hotels.service.models.Hotel;
+import com.expedia.hotels.service.models.HotelDeals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ExpediaApplication.class)
 public class ExpediaApplicationTests {
+	
+	@Autowired
+	private OffersService offerService;
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testExpediaApi() {
+		HotelDeals hotelOffers = offerService.getHotelOffers(null, null, null, null, null, null, null, null, null, null, null, null);
+		Assert.assertNotNull(hotelOffers);
+		Assert.assertNotNull(hotelOffers.getOffers());
+		Assert.assertNotNull(hotelOffers.getUserInfo());
+
+	}
+	
+
+	
+	@Test
+	public void testExpediaApiWithFilter() {
+		HotelDeals hotelOffers = offerService.getHotelOffers(null, null, null, null, null, null, null, null, null, null, "100", null);
+		Assert.assertNotNull(hotelOffers);
+		Assert.assertNotNull(hotelOffers.getOffers());
+		Assert.assertNotNull(hotelOffers.getUserInfo());
+		for(Hotel offer: hotelOffers.getOffers().getHotel()) {
+			Assert.assertNotNull(offer.getHotelPricingInfo());
+			Assert.assertTrue(offer.getHotelPricingInfo().getAveragePriceValue()>=100.0);
+		}
+
 	}
 
 }
