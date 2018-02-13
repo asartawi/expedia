5047ae99220b554c49a070b63475ad40a89c0442
abdulhafeth <abd.sarta@gmail.com>
2018-02-13 18:57:01 +0200

    integrate API filters

--- src/main/java/com/expedia/hotels/controllers/HotelsController.java
+++ src/main/java/com/expedia/hotels/controllers/HotelsController.java
@@ -1,8 +1,9 @@
 package com.expedia.hotels.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,18 @@ public class HotelsController {
 	private OffersService offerService;
 
 	@RequestMapping("/deals")
-	@ResponseBody HotelDeals hotelDeals() {
-		return offerService.getHotelOffers();
+	@ResponseBody HotelDeals hotelDeals(@RequestParam(name="destinationName", required=false) String destinationName,
+			@RequestParam(name="minTripStartDate", required=false) String minTripStartDate,
+			@RequestParam(name="maxTripStartDate", required=false) String maxTripStartDate,
+			@RequestParam(name="minStarRating", required=false) String minStarRating,
+			@RequestParam(name="maxStarRating", required=false) String maxStarRating,
+			@RequestParam(name="minGuestRating", required=false) String minGuestRating,
+			@RequestParam(name="maxGuestRating", required=false) String maxGuestRating,
+			@RequestParam(name="minTotalRate", required=false) String minTotalRate,
+			@RequestParam(name="maxTotalRate", required=false) String maxTotalRate,
+			@RequestParam(name="lengthOfStay", required=false) String lengthOfStay) {
+		return offerService.getHotelOffers(destinationName, minTripStartDate, maxTripStartDate, minStarRating,
+				maxStarRating, minGuestRating, maxGuestRating, minTotalRate, maxTotalRate, lengthOfStay);
 	}
 
 }
--- src/main/java/com/expedia/hotels/service/OffersService.java
+++ src/main/java/com/expedia/hotels/service/OffersService.java
@@ -1,17 +1,62 @@
 package com.expedia.hotels.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.expedia.hotels.service.models.HotelDeals;
 
 @Service
 public class OffersService {
 	
-	public HotelDeals getHotelOffers() {
+	public HotelDeals getHotelOffers(String destinationName, String minTripStartDate, String maxTripStartDate, String minStarRating,
+			String maxStarRating, String minGuestRating, String maxGuestRating, String minTotalRate, String maxTotalRate, String lengthOfStay) {
 		RestTemplate restTemplate = new RestTemplate();
-		String fooResourceUrl = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
-		HotelDeals hotelOffers = restTemplate .getForObject(fooResourceUrl, HotelDeals.class);
+		String offersUrl = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
+
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(offersUrl);
+		if (!StringUtils.isEmpty(destinationName)) {
+			builder.queryParam("destinationName", destinationName);
+		}
+		
+		if (!StringUtils.isEmpty(minTripStartDate)) {
+			builder.queryParam("minTripStartDate", minTripStartDate);
+		}
+		
+		if (!StringUtils.isEmpty(maxTripStartDate)) {
+			builder.queryParam("maxTripStartDate", maxTripStartDate);
+		}
+		
+		if (!StringUtils.isEmpty(minStarRating)) {
+			builder.queryParam("minStarRating", minStarRating);
+		}
+		
+		if (!StringUtils.isEmpty(maxStarRating)) {
+			builder.queryParam("maxStarRating", maxStarRating);
+		}
+
+		if (!StringUtils.isEmpty(minGuestRating)) {
+			builder.queryParam("minGuestRating", minGuestRating);
+		}
+		
+		if (!StringUtils.isEmpty(maxGuestRating)) {
+			builder.queryParam("maxGuestRating", maxGuestRating);
+		}
+		
+		if (!StringUtils.isEmpty(minTotalRate)) {
+			builder.queryParam("minTotalRate", minTotalRate);
+		}
+		
+		if (!StringUtils.isEmpty(maxTotalRate)) {
+			builder.queryParam("maxTotalRate", maxTotalRate);
+		}
+		
+		if (!StringUtils.isEmpty(lengthOfStay)) {
+			builder.queryParam("lengthOfStay", lengthOfStay);
+		}
+		
+		HotelDeals hotelOffers = restTemplate .getForObject(builder.toUriString(), HotelDeals.class);
 		return hotelOffers;
 	}
 
